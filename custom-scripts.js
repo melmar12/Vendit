7dfb2f12ab8da85231a63c7d95d01f20c424744e
Melissa Martinez <melmar12@live.com>
2015-08-08 17:55:06 -0500

    created slick text carousel

--- custom-scripts.js
+++ custom-scripts.js
@@ -1,1 +1,22 @@
+
+// default loaded content 
 $( ".Content" ).load( "home.html" );
+
+$(window).load(function() {
+    // image carousel
+    $('.flexslider').flexslider( {
+        animation: "slide",
+    });
+    
+    // text carousel
+    $('.text-carousel').slick({
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        arrows: true,
+        dots: true,
+    
+
+    });
+    
+    });
